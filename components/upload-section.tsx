5f6453eb994dc916a5804bd88ebb41b8cc5725e5
agssolar <agssolar.in@gmail.com>
2025-04-02 17:11:50 +0530

    api revert

--- components/upload-section.tsx
+++ components/upload-section.tsx
@@ -11,8 +11,7 @@ import { Upload, FileUp } from "lucide-react";
 interface UploadSectionProps {
   isActive: boolean;
 }
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
+const API_URL = "http://54.91.83.80:8080"; // FastAPI backend URL
 
 export default function UploadSection({ isActive }: UploadSectionProps) {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
